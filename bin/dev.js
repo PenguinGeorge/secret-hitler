ab274368ec349728972ff598bebf651dad93f525
George J <PenguinGeorge@users.noreply.github.com>
2021-02-04 19:54:02 +0000

    Update dev.js

--- bin/dev.js
+++ bin/dev.js
@@ -1,7 +1,9 @@
 'use strict';
 
 const http = require('http');
+const https = require('https');
 const express = require('express');
+const fs = require('fs');
 require('dotenv').config();
 const port = (() => {
 	const val = process.env.PORT || '8080';
@@ -17,25 +19,52 @@ const port = (() => {
 
 	return false;
 })();
+const securePort = (() => {
+	const val = process.env.HTTPSPORT || '8443';
+	const port = parseInt(val, 10);
+
+	if (isNaN(port)) {
+		return val;
+	}
+
+	if (port >= 0) {
+		return port;
+	}
+
+	return false;
+})();
 
 global.app = express();
 
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/sh.nitro.wingless.co.uk/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/sh.nitro.wingless.co.uk/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/sh.nitro.wingless.co.uk/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
 const debug = require('debug')('app:server');
-const server = http.createServer(app);
+//const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
 
 global.io = require('socket.io')(server);
 global.notify = require('node-notifier');
 
-app.set('port', port);
+app.set('port', securePort);
 app.set('strict routing', true);
-server.listen(port);
+
+//httpServer.listen(port);
+httpsServer.listen(securePort);
 
 function onError(error) {
 	if (error.syscall !== 'listen') {
 		throw error;
 	}
 
-	const bind = typeof port === 'string' ? 'Pipe ' + port : 'Port ' + port;
+	const bind = typeof securePort === 'string' ? 'Pipe ' + securePort : 'Port ' + securePort;
 
 	switch (error.code) {
 		case 'EACCES':
@@ -52,12 +81,15 @@ function onError(error) {
 }
 
 function onListening() {
-	const addr = server.address();
+	const addr = httpsServer.address();
 	const bind = typeof addr === 'string' ? 'pipe ' + addr : 'port ' + addr.port;
 	debug('Listening on ' + bind);
 	console.log('Listening on ' + bind);
 	require('../app');
 }
 
-server.on('error', onError);
-server.on('listening', onListening);
+//httpServer.on('error', onError);
+//httpServer.on('listening', onListening);
+
+httpsServer.on('error', onError);
+httpsServer.on('listening', onListening);
